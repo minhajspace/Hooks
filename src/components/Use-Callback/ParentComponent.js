2f207a5eec60bd9cd4cf848b1dd98b3358b546da
minhajspace <minhaj.space@gmail.com>
2020-12-22 23:11:37 +0530

    useCallback complete

--- src/components/Use-Callback/ParentComponent.js
+++ src/components/Use-Callback/ParentComponent.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useCallback} from 'react'
 import Title from './Title'
 import Count from './Count'
 import Button from './Button'
@@ -7,12 +7,12 @@ const ParentComponent = () => {
     const [age,setAge]=useState(25)
     const[salary , setSalary] =useState(25000)
 
-    const incrementAge=()=>{
-      return   setAge(age + 1  )
-    }
-    const incrementSalary =() =>{
-     return    setSalary(salary + 1000)
-    }
+    const incrementAge = useCallback(()=>{
+        setAge(age + 1)
+    },[age])
+    const incrementSalary = useCallback(()=>{
+        setSalary(salary + 1000)
+    },[salary])
     return (
         <div> 
             <Title/>
